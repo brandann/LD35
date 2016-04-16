9fc8ebeedf7155131957af96c2a3a37ff5ec5342
brandan <foilpudding@gmail.com>
2016-04-16 11:00:17 -0700

    updated colors on menu and tutorial

--- Assets/Tutorial.cs
+++ Assets/Tutorial.cs
@@ -12,10 +12,10 @@ public class Tutorial : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	    TutorialText = new List<string>();
-        TutorialText.Add("Shawman, a great plague is sweeping the towns! The Villagers from the neighboring tribes are flocking to the sanctuary...");
+        TutorialText.Add("Shaman, a great plague is sweeping the towns! The Villagers from the neighboring tribes are flocking to the sanctuary...");
         TutorialText.Add("The plague is allowing the evil spirits of our ancestors to disguise as our brethren...");
-        TutorialText.Add("Use your ancient Shawman powers of “RIGHT-CLICKING” (whatever that means) to reveal our foes!");
-        TutorialText.Add("Look with your “MOUSE” and “LEFT-CLICK” to destroy the evil among us. Don’t ask me what that means, I’m just a simple gatherer...");
+        TutorialText.Add("Use your ancient Shaman powers of <b><color=teal>“RIGHT-CLICKING”</color></b> (whatever that means) to reveal our foes!");
+        TutorialText.Add("Look with your <b><color=purple>“MOUSE”</color></b> and <b><color=teal>“LEFT-CLICK”</color></b> to destroy the evil among us. Don’t ask me what that means, I’m just a simple gatherer...");
 
         mText.GetComponent<Text>().text = TutorialText[index];
 	}
