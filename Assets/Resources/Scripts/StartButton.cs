16c9377e5581da1fd90d274996194e207d12d6ab
brandan <foilpudding@gmail.com>
2016-04-16 14:07:16 -0700

    More Art, Better start screen

---
+++ Assets/Resources/Scripts/StartButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+    public Button mStartButton;
+    private float mStart;
+    private float mDur = 2.5f;
+    private bool start = false;
+
+    public GameObject BM;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(start)
+        {
+            if((Time.timeSinceLevelLoad - mStart) > mDur)
+            {
+                Application.LoadLevel("Tutorial");
+            }
+        }
+	}
+
+    public void OnStartClick()
+    {
+        mStartButton.gameObject.SetActive(false);
+        mStart = Time.timeSinceLevelLoad;
+        start = true;
+        var go = GameObject.Instantiate(BM);
+        go.transform.position = this.transform.position;
+        go.GetComponent<BurstManager>().mColor = Color.yellow;
+        
+    }
+}
